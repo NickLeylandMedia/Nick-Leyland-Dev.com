ca66f96d7610e50b9dd16ac2c54b492e7b9ed6e0
NickLeylandMedia <nick@nick-leyland.com>
2023-12-11 13:49:10 -0500

    Added google analytics scripts.

--- frontend-v2/src/pages/_app.tsx
+++ frontend-v2/src/pages/_app.tsx
@@ -7,19 +7,17 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
-      {/* <Script
-          id="analytics-1"
-          strategy="lazyOnload"
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-        ></Script>
-        <Script id="analytics-2" strategy="lazyOnload">
-          {`window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-  
-    gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID);`}
-        </Script> */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-VQE7PL298E"
+      ></Script>
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VQE7PL298E');`}
+      </Script>
       <Component {...pageProps} />
     </div>
   );
